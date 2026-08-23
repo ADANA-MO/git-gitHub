d3992d1f2388f131a6abbcb4b23ed67aecf6cc30
ADN DEV <adaninholourd@gmail.com>
2026-08-23 17:02:18 +0100

    add button message

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello From VS code !");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    alert("Git makes changes easy  to track !" );
+});
